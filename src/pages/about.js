adf3812a72e0176247ffd081ec4ac190adc6fc6a
hemantyogananda <hemant123yoga@gmail.com>
2023-02-11 21:50:57 +0530

    added home and about page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>gfhgffhffbvcvbfdgfdfbd dggfhdfgfd</p>
+        <p>gfhgffhffbvcvbfdgfdfbd dggfhdfgfd</p>
+        <p>gfhgffhffbvcvbfdgfdfbd dggfhdfgfd</p>
+        <p>gfhgffhffbvcvbfdgfdfbd dggfhdfgfd</p>
+        <p>gfhgffhffbvcvbfdgfdfbd dggfhdfgfd</p>
+    </div>
+  )
+}
+
+export default about
