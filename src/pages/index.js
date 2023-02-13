8584cad3766c96da3f63ab5226ee35724000e589
hemantyogananda <hemant123yoga@gmail.com>
2023-02-13 11:21:24 +0530

    Added Module styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from 'gatsby'
 import Layout from "../components/Layout"
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
