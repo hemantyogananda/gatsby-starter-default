720caceacb81f9547911f9c17c452e699150d2c3
hemantyogananda <hemant123yoga@gmail.com>
2023-02-13 13:03:22 +0530

    Gatsby V3 styling fixes

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import styles from '../../styles/projects.module.css'
+import { portfolio } from '../../styles/projects.module.css'
 
 const Projects = () => {
   return (
     <Layout>
-      <div className={styles.portfolio}>
+      <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
       </div>
