93a152fc2f5b873ee562f5383ae84faf40e7b538
hemantyogananda <hemant123yoga@gmail.com>
2023-02-13 11:24:18 +0530

    Added button styles in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
-          <Link to="/projects">My Portfolio Projects</Link>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
