b067088aa28d1ed897e1a731cc2fdc737ba4c6e8
hemantyogananda <hemant123yoga@gmail.com>
2023-02-13 09:41:07 +0530

    Added 404 and Project page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <h2>Portfolio</h2>
+        <h3>Projects and Websites I have created</h3>
+
+    </div>
+  )
+}
+
+export default Projects
